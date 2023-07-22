11c62d876c39ea171c69ab89ffb69553ccd522a1
OrlandoUrbanoTrejov <urbanoorlando79@gmail.com>
2023-07-22 13:56:26 -0600

    Determinar si una persona es mayor de edad

---
+++ Ejercicio23.py
@@ -0,0 +1,12 @@
+"""Autor: Orlando Urbano Trejo (Lando)
+   Fecha: 13-07-2023
+   Correo: urbanoorlando79@gmail.com
+
+Ejercicio: Genera un programa que determine si eres mayor de edad """
+
+Edad = int(input("Edad: "))
+if Edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+
