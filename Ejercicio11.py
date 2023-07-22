c9cf8197c6c328fd357c518d03753c277b509e6a
OrlandoUrbanoTrejov <urbanoorlando79@gmail.com>
2023-07-22 13:21:10 -0600

    Multiplicacion de dos numeros

---
+++ Ejercicio11.py
@@ -0,0 +1,11 @@
+"""Autor: Orlando Urbano Trejo (Lando)
+   Fecha: 13-07-2023
+   Correo: urbanoorlando79@gmail.com
+
+Ejercicio: Multiplcar dos numeros"""
+
+Numero1 = float(input("Numero 1:"))
+Numero2 = float(input("Numero 2:"))
+Resultado = Numero1 * Numero2;
+print("Resultado = {:.2f}".format(Resultado))
+
