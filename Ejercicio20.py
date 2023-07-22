4bcfdffdb91e3feff2a7aa8b5b882cac0b616ef5
OrlandoUrbanoTrejov <urbanoorlando79@gmail.com>
2023-07-22 13:45:24 -0600

    Concatenar 3 numeros o letras y mostrar el resultado

---
+++ Ejercicio20.py
@@ -0,0 +1,14 @@
+"""Autor: Orlando Urbano Trejo (Lando) 
+   Fecha: 13-07-2023
+   Correo: urbanoorlando79@gmail.com
+
+Ejercicio: Concatenar 3 numeros o letras y mostrar el resultado"""
+
+N1 = int(input("Número 1: "))
+N2 = int(input("Número 2: "))
+N3 = int(input("Número 3: "))
+
+Resultado = str(N1) + str(N2) + str(N3)
+print("Concatenación:", Resultado)
+
+
