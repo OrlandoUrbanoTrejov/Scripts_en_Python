721c9b944235f318d51cc858bb8446bacd097117
OrlandoUrbanoTrejov <urbanoorlando79@gmail.com>
2023-07-22 12:50:11 -0600

    Calcular la calificacion proporcionada de estudiantes respecto a un rango

---
+++ Ejercicio1.py
@@ -0,0 +1,29 @@
+"""Autor: Orlando Urbano Trejo (Lando) 
+   Fecha: 13-07-2023
+   Correo: urbanoorlando79@gmail.com
+
+ Ejercicio:
+ Realice un algoritmo que, con base en una calificacion proporcionada (0-10), inidique con letra la calificacion que le corresponde: 10 es "A", 9 es "B", 8 es "C", 7 y 6 son "D", y de 5 a 0 son "F".
+ """
+Calificacion = int(input("Calificacion obtenida (0-10):"))
+if(Calificacion == 1):
+     print("Tu calificacion es F")
+elif(Calificacion == 2):
+    print("Tu calificacion es F")
+elif(Calificacion == 3):
+    print("Tu calificacion es F")
+elif(Calificacion == 4):
+    print("Tu calificacion es F")
+elif(Calificacion == 5):
+    print("Tu calificacion es F")
+elif(Calificacion == 6):
+    print("Tu calificacion es D")
+elif(Calificacion == 7):
+    print("Tu calificacion es D")
+elif(Calificacion == 8):
+    print("Tu calificacion es C")
+elif(Calificacion == 9):
+    print("Tu calificacion es B")
+elif(Calificacion == 10):
+    print("Tu calificacion es A")
+
