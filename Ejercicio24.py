8de50e696122e53b4fb6b9883a4bea17964c15c5
OrlandoUrbanoTrejov <urbanoorlando79@gmail.com>
2023-07-22 14:05:24 -0600

    Calcular la tercera parte del doble de un numero

---
+++ Ejercicio24.py
@@ -0,0 +1,12 @@
+"""Autor:  Orlando Urbano Trejo (Lando)
+   Fecha:  17-07-2023
+   Correo: urbanoorlando79@gmail.com
+
+Ejercicio: Realiza un codigo que calcule la mitad de la tercera parte del doble de un numero """
+
+Numero = int(input("Ingresa un numero: "))
+Resul1 = Numero * 2
+Resul2 = (Resul1 / 3) 
+Final = Resul2 /2
+print("La mitad de la tercera parte del doble de", Numero, "es:",Final)
+
